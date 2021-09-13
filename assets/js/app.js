48d50be8f686f5a80de570d920d64a3177fe1415
FarruxTGP <farruhlee02@gmail.com>
2021-09-13 12:14:57 +0500

    m

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,27 @@
 
 
+document.getElementById("head").onwheel = function(){
+    contents.forEach( (item)=>{
+        item.style.display = "none"
+    } )
+    document.getElementById("head").style.display = "flex"
+    document.getElementById("head").style.position = "fixed"
+    document.querySelector(".content>.brend").style.display = "flex"
+}
+document.getElementById("brend").onwheel = function(){
+    contents.forEach( (item)=>{
+        item.style.display = "none"
+    } )
+    document.getElementById("brend").style.display = "flex"
+    document.getElementById("brend").style.position = "fixed"
+    document.querySelector(".content>.onas").style.display = "flex"
+}
 document.getElementById("onas").onwheel = function(){
     contents.forEach( (item)=>{
         item.style.display = "none"
     } )
+    document.getElementById("onas").style.display = "flex"
+    document.getElementById("onas").style.position = "fixed"
     document.querySelector(".content>.chisla").style.display = "flex"
 }
 
@@ -11,6 +29,8 @@ document.getElementById("chisla").onwheel = function(){
     contents.forEach( (item)=>{
         item.style.display = "none"
     } )
+    document.getElementById("chisla").style.display = "flex"
+    document.getElementById("chisla").style.position = "fixed"
     document.querySelector(".content>.group").style.display = "flex"
 }
 
@@ -18,6 +38,10 @@ document.getElementById("group").onwheel = function(){
     contents.forEach( (item)=>{
         item.style.display = "none"
     } )
+    document.getElementById("group").style.display = "flex"
+    document.getElementById("group").style.position = "fixed"
+    document.getElementById("sliders").style.position = "relative"
+
     document.querySelector(".content>.sliders").style.display = "flex"
 }
 
@@ -25,6 +49,8 @@ document.getElementById("sliders").onwheel = function(){
     contents.forEach( (item)=>{
         item.style.display = "none"
     } )
+    document.getElementById("sliders").style.display = "flex"
+    document.getElementById("sliders").style.position = "fixed"
     document.querySelector(".content>.news").style.display = "flex"
 }
 
@@ -32,12 +58,16 @@ document.getElementById("news").onwheel = function(){
     contents.forEach( (item)=>{
         item.style.display = "none"
     } )
+    document.getElementById("news").style.display = "flex"
+    document.getElementById("news").style.position = "fixed"
     document.querySelector(".content>.mapic").style.display = "flex"
 }
 document.getElementById("mapic").onwheel = function(){
     contents.forEach( (item)=>{
         item.style.display = "none"
     } )
+    document.getElementById("mapic").style.display = "flex"
+    document.getElementById("mapic").style.position = "fixed"
     document.querySelector(".content>.footer").style.display = "flex"
 }
 document.getElementById("footer").onwheel = function(){
@@ -57,12 +87,7 @@ document.getElementById("showAbout").onclick = function(){
     } )
     document.querySelector(".content>.onas").style.display = "flex"
 }
-document.getElementById("brend").onwheel = function(){
-    contents.forEach( (item)=>{
-        item.style.display = "none"
-    } )
-    document.querySelector(".content>.onas").style.display = "flex"
-}
+
 document.getElementById("showChisla").onclick = function(){
     contents.forEach( (item)=>{
         item.style.display = "none"
@@ -166,49 +191,10 @@ path.forEach((item)=>{
 })
 
 
-document.getElementById("onas").onwheel = function(){
-    contents.forEach( (item)=>{
-        item.style.display = "none"
-    } )
-    document.querySelector(".content>.chisla").style.display = "flex"
-}
 
-document.getElementById("chisla").onwheel = function(){
-    contents.forEach( (item)=>{
-        item.style.display = "none"
-    } )
-    document.querySelector(".content>.group").style.display = "flex"
-}
-
-document.getElementById("group").onwheel = function(){
-    contents.forEach( (item)=>{
-        item.style.display = "none"
-    } )
-    document.querySelector(".content>.sliders").style.display = "flex"
-}
-
-document.getElementById("sliders").onwheel = function(){
-    contents.forEach( (item)=>{
-        item.style.display = "none"
-    } )
-    document.querySelector(".content>.news").style.display = "flex"
-}
-
-document.getElementById("news").onwheel = function(){
-    contents.forEach( (item)=>{
-        item.style.display = "none"
-    } )
-    document.querySelector(".content>.mapic").style.display = "flex"
-}
-document.getElementById("mapic").onwheel = function(){
-    contents.forEach( (item)=>{
-        item.style.display = "none"
-    } )
-    document.querySelector(".content>.footer").style.display = "flex"
-}
 document.getElementById("footer").onwheel = function(){
     contents.forEach( (item)=>{
         item.style.display = "none"
     } )
-    document.querySelector(".content>.brend").style.display = "flex"
+    document.querySelector(".content>.header_wrapper").style.display = "flex"
 }
